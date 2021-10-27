acb0a4c4200581be60f54eddb83dfe0390981f99
Robin <compsigh.wav@gmail.com>
2021-10-27 09:32:31 -0600

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let fetchLink;
+let currentObject;
+let outputHTML;
+
+const fetchRequest = function(callback) {
+   fetchLink = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + Math.floor(Math.random() * 999);
+   fetch(fetchLink).then(response => {
+      if (response.ok) {
+         return response.json();
+      } else {
+         throw Error(response.statusText);
+      }
+   }).then(response => {
+      currentObject = response;
+   }).then(response => {
+      callback(currentObject);
+   }).catch(e => {
+      console.log(e)
+      outputHTML = `<p class="descriptionText">This museum item does not exist! Press the button below to try again.</p>`;
+  document.querySelector(".item").innerHTML = outputHTML;
+   });
+}
+const objectToHTML = function(currentObject) {
+   outputHTML = `<img src="${currentObject.primaryImageSmall}" alt="${currentObject.title}" class="img">
+   <p class="descriptionText">${currentObject.title}</p>`;
+  document.querySelector(".item").innerHTML = outputHTML;
+}
+
+const newItem= function() {
+   fetchRequest(objectToHTML);
+}
+
+document.getElementById("btn").addEventListener("click", newItem);
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Course Title
+CPNT 262
+
+## Author Name
+Robin (Abishek Roka) Pradhan
+
+## Hints
+This app gets an object for a random item from the The Metropolitan Museum of Art Collection API.
+
+The way they organzed these items is sequential, 
+"https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]"
+
+Sadly, many items are missing so fetching from them returns a 404 error. In this case I've made the page display an item missing message. 
+
+## Links
+https://github.com/compsigh-url/cpnt262-a3/
+https://compsigh-url.github.io/cpnt262-a3/
+
+## Atrributions
+Parts of my work were guided by FreeCodeCamp but all code is original. 
